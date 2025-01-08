480fb8c36fb950e649dd2439d7eb3ed57a36169a
CosecSecCot <cosecseccot581@gmail.com>
2025-01-08 20:54:07 +0530

    reduce gap between footer and content in contact page

--- frontend/src/pages/Contact/index.jsx
+++ frontend/src/pages/Contact/index.jsx
@@ -6,38 +6,40 @@ export default function ContactPage() {
     return (
         <>
             <Navbar />
-            <main className="font-barlow">
-                <article className="mt-[30px] sm:mt-[60px] mx-[31px] sm:mx-[80px] flex justify-center">
-                    <section className="flex gap-[68px] items-center max-w-[1120px] flex-wrap lg:flex-nowrap justify-center">
-                        <div>
-                            <span className="font-agdasima text-[20px] leading-[24px] text-persian-blue">
-                                CONTACT US FOR ANY QUERIES
-                            </span>
-                            <h1 className="text-[48px] leading-[58px]">
-                                Got a question? You can contact us through are
-                                contact information provided below.
-                            </h1>
-                        </div>
-                        <h2 className="font-extralight text-[25px] leading-[34px]">
-                            We are a collective of diverse thinkers reimagining
-                            how human-technology interactions can be seamless
-                            and meaningful.
-                        </h2>
-                    </section>
-                </article>
-                <article className="min-h-[50vh] items-start mt-[30px] mb-[86px] sm:mt-[60px] mx-[31px] sm:mx-[80px] flex justify-center">
-                    <section className="flex gap-[24px] items-center max-w-[1120px] flex-wrap lg:flex-nowrap justify-center">
-                        {new Array(3).fill(0).map((_, idx) => (
-                            <ContactCard
-                                key={idx}
-                                title="sigchi@iiitd.ac.in"
-                                subtitle="We are a collective of diverse thinkers reimagining how human-technology interactions can be seamless and meaningful."
-                            />
-                        ))}
-                    </section>
-                </article>
+            <div className="flex flex-col justify-between gap-[86px] lg:gap-[172px]">
+                <main className="min-h-[50vh] font-barlow">
+                    <article className="mt-[30px] sm:mt-[60px] mx-[31px] sm:mx-[80px] flex justify-center">
+                        <section className="flex gap-[68px] items-center max-w-[1120px] flex-wrap lg:flex-nowrap justify-center">
+                            <div>
+                                <span className="font-agdasima text-[20px] leading-[24px] text-persian-blue">
+                                    CONTACT US FOR ANY QUERIES
+                                </span>
+                                <h1 className="text-[48px] leading-[58px]">
+                                    Got a question? You can contact us through
+                                    are contact information provided below.
+                                </h1>
+                            </div>
+                            <h2 className="font-extralight text-[25px] leading-[34px]">
+                                We are a collective of diverse thinkers
+                                reimagining how human-technology interactions
+                                can be seamless and meaningful.
+                            </h2>
+                        </section>
+                    </article>
+                    <article className="items-start mt-[30px] sm:mt-[60px] mx-[31px] sm:mx-[80px] flex justify-center">
+                        <section className="flex gap-[24px] items-center max-w-[1120px] flex-wrap lg:flex-nowrap justify-center">
+                            {new Array(3).fill(0).map((_, idx) => (
+                                <ContactCard
+                                    key={idx}
+                                    title="sigchi@iiitd.ac.in"
+                                    subtitle="We are a collective of diverse thinkers reimagining how human-technology interactions can be seamless and meaningful."
+                                />
+                            ))}
+                        </section>
+                    </article>
+                </main>
                 <Footer />
-            </main>
+            </div>
         </>
     );
 }
